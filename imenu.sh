7df4dfc05dbc1eaaa21130a88124947e3e5fd1c9
paperbenni <paperbenni@gmail.com>
2020-05-13 15:13:43 +0200

    add list option

--- imenu.sh
+++ imenu.sh
@@ -7,7 +7,7 @@
 case "$1" in
 -c) # confirmation dialog with prompt $2
     echo "yes
-no" | instantmenu -c -w 800 -l 100 -p "${2:-confirm}" >/tmp/isntantanswer
+no" | instantmenu -c -l 100 -p "${2:-confirm}" >/tmp/isntantanswer
     ANSWER="$(cat /tmp/isntantanswer)"
     if grep -q "yes" <<<"${ANSWER}"; then
         exit 0
@@ -21,7 +21,7 @@ no" | instantmenu -c -w 800 -l 100 -p "${2:-confirm}" >/tmp/isntantanswer
 yes
 no"
     while ! grep -Eq '^(yes|no|forcequit)$' <<<"$ANSWER"; do
-        echo "$PROMPT" | instantmenu -c -w 800 -l 100 >/tmp/isntantanswer
+        echo "$PROMPT" | instantmenu -c -l 100 >/tmp/isntantanswer
         ANSWER="$(cat /tmp/isntantanswer)"
     done
     if grep -q "yes" <<<"${ANSWER}"; then
@@ -40,12 +40,32 @@ no"
     PROMPT=$(sed 's/^/> /g' <<<$2)
     PROMPT="$PROMPT
 OK"
-    echo "$PROMPT" | instantmenu -l 20 -w 800 -c
+    echo "$PROMPT" | instantmenu -l 20 -c
     ;;
 -M) # message from stdin
     PROMPT="$(cat /dev/stdin | sed 's/^/> /g')"
     PROMPT="$PROMPT
 OK"
-    echo "$PROMPT" | instantmenu -l 20 -w 800 -c
+    echo "$PROMPT" | instantmenu -l 20 -c
     ;;
+
+-l) # choose item from list
+    ASTDIN="$(cat /dev/stdin)"
+    ANSWER=""
+
+    while ! grep -q .. <<<"$ANSWER"; do
+        ANSWER=$(echo "$ASTDIN" | instantmenu -p "${2:-choose}" -c -l 20)
+
+        if grep -q "forcequit" <<<"$ANSWER"; then
+            exit 1
+        fi
+
+        if ! grep -q "^$ANSWER$" <<<"$ASTDIN"; then
+            ANSWER=""
+        fi
+    done
+
+    echo "$ANSWER"
+    ;;
+
 esac
