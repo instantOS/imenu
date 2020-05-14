d7deb1f2cffc18b80e0b050c33c3c7bbf794101a
paperbenni <paperbenni@gmail.com>
2020-05-14 15:30:25 +0200

    add menu border

--- imenu.sh
+++ imenu.sh
@@ -7,7 +7,7 @@
 case "$1" in
 -c) # confirmation dialog with prompt $2
     echo "yes
-no" | instantmenu -c -l 100 -p "${2:-confirm}" >/tmp/isntantanswer
+no" | instantmenu -bw 4 -c -l 100 -p "${2:-confirm}" >/tmp/isntantanswer
     ANSWER="$(cat /tmp/isntantanswer)"
     if grep -q "yes" <<<"${ANSWER}"; then
         exit 0
@@ -21,7 +21,7 @@ no" | instantmenu -c -l 100 -p "${2:-confirm}" >/tmp/isntantanswer
 yes
 no"
     while ! grep -Eq '^(yes|no|forcequit)$' <<<"$ANSWER"; do
-        echo "$PROMPT" | instantmenu -c -l 100 >/tmp/isntantanswer
+        echo "$PROMPT" | instantmenu -bw 4 -c -l 100 >/tmp/isntantanswer
         ANSWER="$(cat /tmp/isntantanswer)"
     done
     if grep -q "yes" <<<"${ANSWER}"; then
@@ -31,22 +31,22 @@ no"
     fi
     ;;
 -P) # password dialog
-    echo "" | instantmenu -p "${2:-enter password}" -P -c -w 800
+    echo "" | instantmenu -bw 4 -p "${2:-enter password}" -P -c -w 800
     ;;
 -i) # input dialog
-    echo "" | instantmenu -p "${2:-enter text}" -c -w 800
+    echo "" | instantmenu -bw 4 -p "${2:-enter text}" -c -w 800
     ;;
 -m) # message
     PROMPT=$(sed 's/^/> /g' <<<$2)
     PROMPT="$PROMPT
 OK"
-    echo "$PROMPT" | instantmenu -l 20 -c
+    echo "$PROMPT" | instantmenu -bw 4 -l 20 -c
     ;;
 -M) # message from stdin
     PROMPT="$(cat /dev/stdin | sed 's/^/> /g')"
     PROMPT="$PROMPT
 OK"
-    echo "$PROMPT" | instantmenu -l 20 -c
+    echo "$PROMPT" | instantmenu -bw 4 -l 20 -c
     ;;
 
 -l) # choose item from list
@@ -54,7 +54,7 @@ OK"
     ANSWER=""
 
     while ! grep -q .. <<<"$ANSWER"; do
-        ANSWER=$(echo "$ASTDIN" | instantmenu -p "${2:-choose}" -c -l 20)
+        ANSWER=$(echo "$ASTDIN" | instantmenu -bw 4 -p "${2:-choose}" -c -l 20)
 
         if grep -q "forcequit" <<<"$ANSWER"; then
             exit 1
